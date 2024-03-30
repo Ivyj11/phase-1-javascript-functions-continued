27da2c7d0198b31e73a89e44afcabc8ab13d3c78
IVY <ivyjkemboi@gmail.com>
2024-03-30 16:21:23 +0300

    learntinnerfunctions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*"){
+
+   // wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+
+    const innerFunction = function (comp = "special") {
+        return `You are ${flair}${comp}${flair}!`;
+      };
+      return innerFunction;
+}
